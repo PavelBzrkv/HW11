4180c24f0e9062ac78c3d1905cea09b6dee890b5
PavelBzrkv <pavel95959@gmail.com>
2025-05-04 00:22:22 +0500

    Phone example

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        Phone phone1 = new Phone();
+        Phone phone2 = new Phone(891267400, "Nokia", 167);
+
+        phone1.receiveCall("Vasay");
+
+        System.out.println(phone1.country);
+        System.out.println(phone2.country);
+
+        phone2.receiveCall("Petya", phone2.phoneNumber);
 
     }
 }
